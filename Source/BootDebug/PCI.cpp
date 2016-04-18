31808ba328951d81f840e29f87bd35e1eb367939
Fox Cutter <foxcutter@gmail.com>
2016-04-18 10:55:34 -0700

    Added device class names to the PCI information.

--- Source/BootDebug/PCI.cpp
+++ Source/BootDebug/PCI.cpp
@@ -8,6 +8,193 @@
 #include "HardwareTree.h"
 #include "CoreComplex.h"
 
+#include <algorithm>
+
+struct PCINameEntry
+{
+	uint32_t Code;
+	char * Name;
+};
+
+PCINameEntry DeviceNameList[] = 
+{
+	// Class 0
+	{0x000100, "VGA-Compatible Device"},
+
+	// Class 1
+	{0x010000, "SCSI Bus Controller"},
+	{0x010100, "IDE Controller"},
+	{0x010200, "Floppy Disk Controller"},
+	{0x010300, "IPI Bus Controller"},
+	{0x010400, "RAID Controller"},
+	{0x010500, "ATA Controller"},
+	{0x010600, "Serial ATA"},
+	{0x010700, "Serial Attached SCSI"},
+	{0x018000, "Other Mass Storage"},
+
+	// Class 2
+	{0x020000, "Ethernet Controller"},
+	{0x020100, "Token Ring Controller"},
+	{0x020200, "FDDI Controller"},
+	{0x020300, "ATM Controller"},
+	{0x020400, "ISDN Controller"},
+	{0x020500, "WorldFip Controller"},
+	{0x020600, "PICMG 2.14 Multi Computing"},
+	{0x028000, "Other Network Controller"},
+
+	// Class 3
+	{0x030000, "VGA-Compatible Controller"},
+	{0x030001, "8512-Compatible Controller"},
+	{0x030100, "XGA Controller"},
+	{0x030200, "3D Controller (Not VGA-Compatible)"},
+	{0x038000, "Other Display Controller "},
+
+	// Class 4
+	{0x040000, "Video Device"},
+	{0x040100, "Audio Device"},
+	{0x040200, "Computer Telephony Device"},
+	{0x040400, "High Definition Audio Device"},
+	{0x048000, "Other Multimedia Controller"},
+
+	// Class 5
+	{0x050000, "RAM Controller"},
+	{0x050100, "Flash Controller"},
+	{0x058000, "Other Memeory Controller"},
+
+	// Class 6
+	{0x060000, "Host Bridge"},
+	{0x060100, "ISA Bridge"},
+	{0x060200, "EISA Bridge"},
+	{0x060300, "MCA Bridge"},
+	{0x060400, "PCI-to-PCI Bridge"},
+	{0x060500, "PCMCIA Bridge"},
+	{0x060600, "NuBus Bridge"},
+	{0x060700, "CardBus Bridge"},
+	{0x060800, "RACEway Bridge"},
+	{0x060940, "PCI-to-PCI Bridge"},
+	{0x060A00, "InfiniBrand-to-PCI Host Bridge "},	
+	{0x068000, "Other Bridge Device"},
+
+	// Class 7
+	{0x070000, "Generic XT-Compatible Serial Controller"},
+	{0x070100, "Parallel Port"},
+	{0x070101, "Bi-Directional Parallel Port"},
+	{0x070102, "ECP 1.X Compliant Parallel Port"},
+	{0x070103, "IEEE 1284 Controller"},
+	{0x0701FE, "IEEE 1284 Target Device"},
+	{0x070200, "Multiport Serial Controller"},
+	{0x070300, "Generic Modem"},
+	{0x070400, "IEEE 488.1/2 (GPIB) Controller"},
+	{0x070500, "Smart Card"},
+	{0x078000, "Other Communications Device"},
+
+	// Class 8
+	{0x080000, "Generic 8259 PIC"},
+	{0x080001, "ISA PIC"},
+	{0x080002, "EISA PIC"},
+	{0x080010, "I/O APIC Interrupt Controller"},
+	{0x080020, "I/O(x) APIC Interrupt Controller"},
+	{0x080100, "Generic 8237 DMA Controller"},
+	{0x080101, "ISA DMA Controller"},
+	{0x080102, "EISA DMA Controller"},
+	{0x080200, "Generic 8254 System Timer"},
+	{0x080201, "ISA System Timer"},
+	{0x080202, "EISA System Timer"},
+	{0x080300, "Generic RTC Controller"},
+	{0x080301, "ISA RTC Controller"},
+	{0x080400, "Generic PCI Hot-Plug Controller"},
+	{0x088000, "Other System Peripheral"},
+	
+	// Class 9
+	{0x090000, "Keyboard Controller"},
+	{0x090100, "Digitizer"},
+	{0x090200, "Mouse Controller"},
+	{0x090300, "Scanner Controller"},
+	{0x090400, "Gameport Controller"},
+	{0x098000, "Other Input Controller"},
+
+	// Class A
+	{0x0A0000, "Generic Docking Station"},
+	{0x0A8000, "Other Docking Station"},
+
+	// Class B
+	{0x0B0000, "386 Processor"},
+	{0x0B0100, "486 Processor"},
+	{0x0B0200, "Pentium Processor"},
+	{0x0B1000, "Alpha Processor"},
+	{0x0B2000, "PowerPC Processor"},
+	{0x0B3000, "MIPS Processor"},
+	{0x0B4000, "Co-Processor"},
+	
+	// Class C
+	{0x0C0000, "IEEE 1394 Controller (FireWire)"},
+	{0x0C0010, "IEEE 1394 Controller (OpenHCI)"},
+	{0x0C0100, "ACCESS.bus"},
+	{0x0C0200, "SSA"},
+	{0x0C0300, "USB1 UHCI Controller"},
+	{0x0C0310, "USB1 OHCI Controller"},
+	{0x0C0320, "USB2 EHCI Controller"},
+	{0x0C0330, "USB3 XHCI Controller"},
+	{0x0C0380, "USB Controller"},
+	{0x0C03FE, "USB (Not Host Controller)"},
+	{0x0C0400, "Fibre Channel"},
+	{0x0C0500, "SMBus"},
+	{0x0C0600, "InfiniBand"},
+	{0x0C0700, "IPMI SMIC Interface"},
+	{0x0C0800, "SERCOS Interface Standard (IEC 61491)"},
+	{0x0C0900, "CANbus"},
+	
+	// Class D
+	{0x0D0000, "iRDA Compatible Controller"},
+	{0x0D0100, "Consumer IR Controller"},
+	{0x0D1000, "RF Controller"},
+	{0x0D1100, "Bluetooth Controller"},
+	{0x0D1200, "Broadband Controller"},
+	{0x0D2000, "Ethernet Controller (802.11a)"},
+	{0x0D2100, "Ethernet Controller (802.11b)"},
+	{0x0D8000, "Other Wireless Controller"},
+	
+	// Class E
+	{0x0E0000, "Intelligent I/O Controllers"},
+	
+	// Class F
+	{0x0F0100, "TV Controller"},
+	{0x0F0200, "Audio Controller"},
+	{0x0F0300, "Voice Controller"},
+	{0x0F0400, "Data Controller"},
+	
+	// Class 10
+	{0x100000, "Network and Computing Encrpytion/Decryption"},
+	{0x101000, "Entertainment Encryption/Decryption"},
+	{0x108000, "Other Encryption/Decryption"},
+	
+	// Class 11
+	{0x110000, "DPIO Modules"},
+	{0x110100, "Performance Counters"},
+	{0x111000, "Communications Syncrhonization Plus Time and Frequency Test/Measurment"},
+	{0x112000, "Management Card"},
+	{0x118000, "Other Data Acquisition/Signal Processing Controller"},
+};
+
+
+
+PCINameEntry * FindPCIClassName(uint32_t ClassCode)
+{
+	// Start with hte full classes code
+	PCINameEntry * Entry = std::find_if(std::begin(DeviceNameList), std::end(DeviceNameList), [ClassCode](PCINameEntry &Entry){return Entry.Code == ClassCode;});
+	if(Entry != std::end(DeviceNameList))
+		return Entry;
+
+	// Remove the interface ID from the class code to get a more generic answer.
+	ClassCode = ClassCode & 0xFFFF00;
+	Entry = std::find_if(std::begin(DeviceNameList), std::end(DeviceNameList), [ClassCode](PCINameEntry &Entry){return Entry.Code == ClassCode;});
+
+	if(Entry != std::end(DeviceNameList))
+		return Entry;
+
+	return nullptr;
+}
+
 PCI::PCI(void)
 {
 }
@@ -104,7 +291,7 @@ bool PCI::DumpBus(uint8_t Bus)
 	uint32_t CurrentDeviceID = 0;
 
 	bool AllFunctions = false;
-	//KernalPrintf("Bus %u\n", Bus);
+
 	while(true)
 	{
 		if(CurrentFunction == 0x08)
@@ -133,14 +320,22 @@ bool PCI::DumpBus(uint8_t Bus)
 		}
 		else
 		{
-			KernalPrintf("PCI %06X(%02X:%02X:%02X)", CurrentDeviceID, Bus, CurrentDevice, CurrentFunction);
+			KernalPrintf(" %06X (%02X:%02X:%02X)", CurrentDeviceID, Bus, CurrentDevice, CurrentFunction);
 			KernalPrintf(" Dev %04X:%04X", Val & 0xFFFF, (Val & 0xFFFF0000) >> 16);
 			
 			Val = ReadRegister(CurrentDeviceID, 0x08);
 			KernalPrintf(", C %02X-%02X-%02X", (Val & 0xFF000000) >> 24, (Val & 0x00FF0000) >> 16, (Val & 0x0000FF00) >> 8);
 			
-			Val = ReadRegister(CurrentDeviceID, 0x2C);
-			KernalPrintf(", Sub %04X:%04X", Val & 0xFFFF, (Val & 0xFFFF0000) >> 16);
+			//Val = ReadRegister(CurrentDeviceID, 0x2C);
+			//KernalPrintf(", Sub %04X:%04X", Val & 0xFFFF, (Val & 0xFFFF0000) >> 16);
+
+			// Get the class code
+			Val = ReadRegister(CurrentDeviceID, 0x08) >> 8;
+			PCINameEntry * Entry = FindPCIClassName(Val);
+
+			if(Entry != nullptr)
+				KernalPrintf(" %.25s", Entry->Name);
+
 
 			Val = ReadRegister(CurrentDeviceID, 0x0C);			
 			Val = (Val & 0x00FF0000) >> 16;
@@ -166,6 +361,7 @@ bool PCI::DumpBus(uint8_t Bus)
 				if(SecondaryBus != SubBus)
 					KernalPrintf("-%02X", SubBus);
 			}
+
 			KernalPrintf("\n");
 		}
 	} 
@@ -218,6 +414,12 @@ bool PCI::DumpDevice(uint32_t DeviceID)
 
 	Val = ReadRegister(DeviceID, 8);
 	KernalPrintf("  Class %02X, Sub Class %02X, Prog IF %02X, Revision ID %02X\n", (Val & 0xFF000000) >> 24, (Val & 0x00FF0000) >> 16, (Val & 0x0000FF00) >> 8, Val & 0x000000FF);
+	// Get the class code
+	Val = Val >> 8;
+	PCINameEntry * Entry = FindPCIClassName(Val);
+
+	if(Entry != nullptr)
+		KernalPrintf("    %s\n", Entry->Name);
 	
 	Val = ReadRegister(DeviceID, 0x0C);
 	KernalPrintf("  BIST %02X, Header Type %02X, Latency Timer %02X, Cache Line Size %02X\n", (Val & 0xFF000000) >> 24, (Val & 0x00FF0000) >> 16, (Val & 0x0000FF00) >> 8, Val & 0x000000FF);

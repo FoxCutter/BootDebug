c09263afd0505d6fddc65a97b57752db98ffb764
Fox Cutter <FoxCutter@gmail.com>
2016-04-28 23:51:55 -0700

    Tweaking the batch file

---
+++ Source/BootDebug/finalbuild-example.bat
@@ -0,0 +1,24 @@
+@echo off
+
+set ToolFolder=%~dp1..\..\Tools
+set ImagePath=D:\VMs\Boot Debug\Grub-real.vhd
+set OutputPath=(hd1)\Kernal.exe
+
+%ToolFolder%\HeaderSet %1
+
+rem echo %ERRORLEVEL%
+IF ERRORLEVEL 0 goto DiskTools
+
+echo HearderSet : Error: can't update header
+goto end
+
+:DiskTools
+%ToolFolder%\DiskTools add "%ImagePath%" "%1" "%OutputPath%"
+IF ERRORLEVEL 0 goto end
+
+echo DiskTools : Error: can't update image
+goto end
+
+
+:end
+
